25bb5aa67391915851e55ab439a877d46f327bf3
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-12-02 12:10:00 +0700

    First day done

---
+++ day-1/index.ts
@@ -0,0 +1,50 @@
+import fs from "fs";
+
+function createRowsFromInput(path: string) {
+  const left: number[] = [];
+  const right: number[] = [];
+
+  const rows = fs
+    .readFileSync("./input.txt", "utf-8")
+    .split("\n")
+    .map((el) => el.split("   "));
+
+  rows.forEach((el) => {
+    left.push(parseInt(el[0]));
+    right.push(parseInt(el[1]));
+  });
+
+  return {
+    left: left.sort(),
+    right: right.sort(),
+  };
+}
+
+const { left, right } = createRowsFromInput("./input.txt");
+
+function compareRows({ left, right }: { left: number[]; right: number[] }) {
+  if (left.length !== right.length) {
+    throw new Error("Rows must be same length");
+  }
+
+  let totalDiff = 0;
+
+  left.forEach((el, i) => {
+    const leftEl = el;
+    const rightEl = right[i];
+
+    const difference = Math.abs(rightEl - leftEl);
+
+    totalDiff += difference;
+  });
+
+  return totalDiff;
+}
+
+console.log(
+  compareRows({
+    left,
+    right,
+  })
+);
+// Answer: 2192892
