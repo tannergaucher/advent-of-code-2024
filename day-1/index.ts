574c324ff288eff811e5d275d2ce942900405e12
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-12-07 16:21:56 +0700

    Improve

--- day-1/index.ts
+++ day-1/index.ts
@@ -4,23 +4,19 @@ export function createRowsFromInput(path: string) {
   const left: number[] = [];
   const right: number[] = [];
 
-  const rows = fs
-    .readFileSync(path, "utf-8")
+  fs.readFileSync(path, "utf-8")
     .split("\n")
-    .map((el) => el.split("   "));
-
-  rows.forEach((el) => {
-    left.push(parseInt(el[0]));
-    right.push(parseInt(el[1]));
-  });
+    .map((el) => el.split("   "))
+    .forEach((el) => {
+      left.push(parseInt(el[0]));
+      right.push(parseInt(el[1]));
+    });
 
   return {
     left: left.sort(),
     right: right.sort(),
   };
 }
-
-const { left, right } = createRowsFromInput("./input.txt");
 
 export function compareRows({
   left,
@@ -29,28 +25,11 @@ export function compareRows({
   left: number[];
   right: number[];
 }) {
-  if (left.length !== right.length) {
-    throw new Error("Rows must be same length");
-  }
-
   let totalDiff = 0;
 
   left.forEach((el, i) => {
-    const leftEl = el;
-    const rightEl = right[i];
-
-    const difference = Math.abs(rightEl - leftEl);
-
-    totalDiff += difference;
+    totalDiff += Math.abs(right[i] - el);
   });
 
   return totalDiff;
 }
-
-console.log(
-  compareRows({
-    left,
-    right,
-  })
-);
-// Answer: 2192892
